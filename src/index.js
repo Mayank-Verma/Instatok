a227a0456ca90dfddd7be39583085540eb24cc9d
Your Name <you@example.com>
2024-08-21 14:30:13 +0530

    renamed uploadRoutes to postRoutes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import uploadRoutes from './routes/uploadRoutes.js';
+import  postRoutes from './routes/postRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
 const app = express();
 app.use(express.json());
 // Routes
-app.use('/api/v1', uploadRoutes);
+app.use('/api/v1',postRoutes );
 app.use('/api/v1', userRoutes);
 
 // Error Handling Middleware
