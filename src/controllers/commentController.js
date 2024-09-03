5577b2ace363b2b70de2595eea26b52aeb4a893b
Your Name <you@example.com>
2024-09-03 19:02:04 +0530

    add comment api implemented

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -6,15 +6,28 @@ export const addPostComment = async (req, res) => {
       status: "success",
       message: "comment added successfully on post!",
     });
+  else if (result === null)
+    res.status(400).json({
+      status: "failed",
+      message: "comment is required!",
+    });
   else
     res.status(500).json({
       status: "failed",
       message: "Something went wrong!",
     });
 };
-export const getPostComments = (req, res) => {
-  res.status(200).json({
-    status: "pending",
-    message: "Work in progress, this feature will be available soon!",
-  });
+export const getPostComments = async (req, res) => {
+  const result = await commentService.getPostComments(req);
+  if (result)
+    res.status(200).json({
+      status: "success",
+      message: "comments on post retrieved successfully!",
+      comments: result,
+    });
+  else
+    res.status(500).json({
+      status: "failed",
+      message: "Something went wrong!",
+    });
 };
--- src/services/commentService.js
+++ src/services/commentService.js
@@ -1,1 +1,22 @@
-export function addPostComment() {}
+import { Model } from "sequelize";
+import Comment from "../models/comments.js";
+import Post from "../models/post.js";
+import verifyTokenFromAuthorizationAndGetPayload from "../utils/verifyTokenFromAuthorizationAndGetPayload.js";
+
+export async function addPostComment(req) {
+  const { id: userId } = verifyTokenFromAuthorizationAndGetPayload(
+    req.headers.authorization
+  );
+  const postId = req.params.postId;
+  const comment = req.body.comment;
+  if (comment == null) return null;
+  const data = await Comment.create({ postId, userId, comment });
+  if (data) return true;
+  else false;
+}
+
+export async function getPostComments(req) {
+  const { id: userId } = verifyTokenFromAuthorizationAndGetPayload(
+    req.headers.authorization
+  );
+}
