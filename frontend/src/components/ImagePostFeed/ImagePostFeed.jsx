c51bf6d83845b69a3d9a97d10a924b520ad32a9f
Your Name <you@example.com>
2024-11-13 18:57:48 +0530

    working on create post

--- frontend/src/components/ImagePostFeed/ImagePostFeed.jsx
+++ frontend/src/components/ImagePostFeed/ImagePostFeed.jsx
@@ -1,26 +1,19 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ImagePost from "../ImagePost/ImagePost";
 import { fetchImagePosts } from "@/api/postService";
+import ReactLoading from "react-loading";
 
 export default function ImagePostFeed() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [token, setToken] = useState("");
 
-  useEffect(() => {
-    // Read the token from localStorage
-    const savedToken = localStorage.getItem("accessToken");
-
-    if (savedToken) {
-      setToken(savedToken); // Update the token in state
-    }
-  }, []);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetchImagePosts();
         const posts = response.posts;
+        setData(posts);
       } catch (error) {
         setError(error);
       } finally {
@@ -29,15 +22,13 @@ export default function ImagePostFeed() {
     };
 
     fetchData();
-  }, [token]); // Empty dependency array means it runs once on mount
+  }, []); // Empty dependency array means it runs once on mount
   // Check if data is null or if posts is not available
   // console.log("data->", data);
   // console.log("data.posts->", data.posts);
-  if (!data || !data.posts) {
+  if (!data || data.length === 0) {
     return <div>No posts available.</div>;
   }
-
-  let posts = data.posts;
 
   return (
     <div>
@@ -46,9 +37,11 @@ export default function ImagePostFeed() {
         // <h1>hi</h1>
         <ImagePost />
       ))} */}
-      {posts.map((post) => (
-        <ImagePost key={post.id} postInfo={post} />
-      ))}
+      {loading ? (
+        <ReactLoading type="spin" color="#36d7b7" height={50} width={50} />
+      ) : (
+        data.map((post) => <ImagePost key={post.id} postInfo={post} />)
+      )}
     </div>
   );
 }
